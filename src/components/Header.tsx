412180d50fe11888ff65b62ffbe875220a8e65c2
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-20 01:22:31 +0200

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import '../styles/Header.css'
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><img src="./sportsee.svg" width="57px" height="57px" alt="sportsees logo"/>Main Title</li>
+                    <li>Accueil</li>
+                    <li>Profil</li>
+                    <li>Réglages</li>
+                    <li>Communauté</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
