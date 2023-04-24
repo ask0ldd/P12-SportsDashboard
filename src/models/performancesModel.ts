627eb60918b54292a774be76925d4dbda24cd8ac
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-24 08:19:50 +0200

    model score fname getters

--- src/models/performancesModel.ts
+++ src/models/performancesModel.ts
@@ -9,14 +9,18 @@ class PerformanceDatas {
     userPerformances : userPerformances
 
     constructor(userId : number){
-        let mDatas = (USER_MAIN_DATA.filter((data : mainDatas) => data.id === userId))[0]
-        this.mainDatas = mDatas
-        let activity = (USER_ACTIVITY.filter((data : userActivity) => data.userId === userId))[0]
-        this.userActivity = activity
-        let session = (USER_AVERAGE_SESSIONS.filter((data : averageSessions) => data.userId === userId))[0]
-        this.userSession = session
-        let performances = (USER_PERFORMANCE.filter((data : userPerformances) => data.userId === userId))[0]
-        this.userPerformances = performances
+        this.mainDatas = (USER_MAIN_DATA.filter((data : mainDatas) => data.id === userId))[0]
+        this.userActivity = (USER_ACTIVITY.filter((data : userActivity) => data.userId === userId))[0]
+        this.userSession = (USER_AVERAGE_SESSIONS.filter((data : averageSessions) => data.userId === userId))[0]
+        this.userPerformances = (USER_PERFORMANCE.filter((data : userPerformances) => data.userId === userId))[0]
+    }
+
+    get score () {
+        return this.mainDatas.score || this.mainDatas.todayScore
+    }
+
+    get firstName(){
+        return this.mainDatas.userInfos.firstName
     }
 }
 
