437455cc44285701bfa152e0c97d42f6b4763d49
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-24 08:09:37 +0200

    model interfaces done / on model

--- src/models/performancesModel.ts
+++ src/models/performancesModel.ts
@@ -1,14 +1,19 @@
 import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../mocks/datas'
-import { userinfos, nutridatas, mainDatas } from '../types/modelTypes'
+import { mainDatas, userActivity, userPerformances, averageSessions } from '../types/modelTypes'
 
 class PerformanceDatas {
 
     mainDatas : mainDatas
+    userActivities : userPerformances
 
     constructor(userId : number){
         // console.log(USER_MAIN_DATA.filter(data => data.id === userId))
-        let datas = (USER_MAIN_DATA.filter((data : mainDatas) => data.id === userId))[0]
-        this.mainDatas = datas
+        let mDatas = (USER_MAIN_DATA.filter((data : mainDatas) => data.id === userId))[0]
+        this.mainDatas = mDatas
+        let activity = (USER_PERFORMANCE.filter((data : userPerformances) => data.userId === userId))[0]
+        this.userActivities = activity
+        let session = (USER_AVERAGE_SESSIONS.filter((data : averageSessions) => data.userId === userId))[0]
+
     }
 }
 
--- src/types/modelTypes.ts
+++ src/types/modelTypes.ts
@@ -1,3 +1,7 @@
+/* */
+/* mainDatas interface */
+/* */
+
 export interface userinfos {
     firstName : string
     lastName : string
@@ -19,6 +23,9 @@ export interface mainDatas {
     keyData: nutridatas
 }
 
+/* */
+/* userActivity interface */
+/* */
 export interface session {
     day : Date
     kilogram : number
@@ -28,4 +35,42 @@ export interface session {
 export interface userActivity {
     userId : number
     sessions : Array<session>
+}
+
+/* */
+/* averageSessions interface */
+/* */
+
+export interface sessionAvg {
+    day : number
+    sessionLength : number
+}
+
+export interface averageSessions {
+    userId : number
+    sessions : Array<sessionAvg>
+}
+
+/* */
+/* userPerformances interface */
+/* */
+
+export interface performance {
+    value:number
+    kind:any
+}
+
+export interface kind {
+    1: string
+    2: string
+    3: string
+    4: string
+    5: string
+    6: string
+}
+
+export interface userPerformances {
+    userId : number
+    kind : kind
+    data : Array<performance>
 }
