14d4f9928366060ccfa7b8020aa11e159dcba7f3
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-24 22:05:49 +0200

    blankdatas mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from './mocks/datas'
 import VerticalMenu from './components/VerticalMenu/VerticalMenu'
 import Greetings from './components/Greetings'
 import NutriDatas from './components/NutriDatas/NutriDatas'
@@ -10,14 +8,12 @@ import AvgSessionChart from './components/AvgSessionChart'
 import PolarChart from './components/PolarChart'
 import ScoreChart from './components/ScoreChart'
 import PerformanceModel from './models/performancesModel'
+import { blankNutridatas, blankSessions } from './mocks/blankDatas'
 
 function App() {
-  const [count, setCount] = useState(0)
   const userId = 1
 
   const userDatas = new PerformanceModel(18)
-
-  console.log(userDatas.avgSessions)
 
   return (
     <div className="App">
@@ -30,12 +26,12 @@ function App() {
             <div className='graphs-container'>
               <DailyActivityChart userId={userId}/>
               <div className='graphsquares-container'>
-                <AvgSessionChart avgSessions={userDatas.avgSessions}/>
+                <AvgSessionChart avgSessions={userDatas.avgSessions ? userDatas.avgSessions : blankSessions}/>
                 <PolarChart userId={userId}/>
                 <ScoreChart score={userDatas.score ? userDatas.score : 0}/>
               </div>
             </div>
-            <NutriDatas nutriDatas={userDatas.nutriDatas ? userDatas.nutriDatas : {calorieCount: 0, proteinCount: 0, carbohydrateCount: 0, lipidCount: 0}}/>
+            <NutriDatas nutriDatas={userDatas.nutriDatas ? userDatas.nutriDatas : blankNutridatas}/>
           </div>
         </section>
       </main>
---
+++ src/mocks/blankDatas.ts
@@ -0,0 +1,37 @@
+export const blankSessions = [
+        {
+            day: 'L',
+            sessionLength: 0
+        },
+        {
+            day: 'M',
+            sessionLength: 0
+        },
+        {
+            day: 'M',
+            sessionLength: 0
+        },
+        {
+            day: 'J',
+            sessionLength: 0
+        },
+        {
+            day: 'V',
+            sessionLength: 0
+        },
+        {
+            day: 'S',
+            sessionLength: 0
+        },
+        {
+            day: 'D',
+            sessionLength: 0
+        }
+]
+
+export const blankNutridatas = {
+    calorieCount: 0, 
+    proteinCount: 0, 
+    carbohydrateCount: 0, 
+    lipidCount: 0
+}
