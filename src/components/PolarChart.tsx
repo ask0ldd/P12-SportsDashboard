0dd878bbf60c14fc0af1f9a0e63b5bf089d03202
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-17 18:33:55 +0200

    polarchart customtick position containerWidth<260

--- src/components/PolarChart.tsx
+++ src/components/PolarChart.tsx
@@ -2,7 +2,7 @@ import { ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, PolarRadius
 import "../styles/PolarChart.css"
 import { IPerformance } from "../types/modelTypes"
 import PropTypes from 'prop-types'
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from "react"
 
 interface props {
     perfDatas : Array<IPerformance>
@@ -34,20 +34,27 @@ const CustomPositionTicks = (props: any) => { // improve props typing
     let dy = 0
     switch(props.payload.value){
       case 'endurance' :
-        dy = 8; dx = -24;
+        dy = 8; 
+        dx= -24;
       break;
       case 'intensité' :
-        dy = -2; dx = -20;
+        dy = -2; 
+        dx = -20;
       break;
       case 'force' :
         dy = 6;
+        dx = props?.containerWidth > 260 ? 0 : -2;
       break;
       case 'cardio' :
-        dx = -28;
+        dx = props?.containerWidth > 260 ? -28 : -22;
       break;
       case 'energie' :
-        dy = 6 ; dx = -32;
-      break;      
+        dy = props?.containerWidth > 260 ? 6 : 10; 
+        dx = props?.containerWidth > 260 ? -32 : -22;
+      break;
+      case 'vitesse' :
+        dx = props?.containerWidth > 260 ? -0 : -10;
+      break;  
     }
     return (
       <g transform={`translate(${props.x},${props.y})`}>
@@ -71,37 +78,55 @@ const PolarChart = ({perfDatas} : props) => {
   const containerRef = useRef<any>()
   const [containerWidth, setContainerWidth] = useState()
 
+  useEffect(() => {
+
+    if(!containerRef.current) { 
+        return;
+    }
+
+    const resizeObserver = new ResizeObserver(() => {
+        if(containerRef.current.current.offsetWidth !== containerWidth) {
+            setContainerWidth(containerRef.current.current.offsetWidth)
+        }
+    })
+
+    resizeObserver.observe(containerRef.current.current)
+
+    return function cleanup() {
+        resizeObserver.disconnect();
+    }
+
+  }, [containerRef.current])
+
   return(
-      /*<article>*/
-          <ResponsiveContainer width="99%" height={263} className="polarchart-container">
-              <RadarChart cx="50%" cy="50%" innerRadius="0%" outerRadius="80%" data={perfDatas}>
-              <PolarGrid
-                  radialLines={false} 
-                  />
-                  <PolarRadiusAxis
-                  tickCount={6}
-                  tick={false}
-                  axisLine={false} 
-                  tickLine={false}
-                  />
-                  <PolarAngleAxis 
-                  dataKey="kind" 
-                  axisLine={false} 
-                  tickLine={false}
-                  tick={<CustomPositionTicks/>}
-                  /* tickFormatter={customEnduranceTick} */
-                  /*tick={{ fontSize: '10px', fill:'#FFFFFF' }}*/
-                  allowDuplicatedCategory={false}/>
-                  <Radar dataKey="value" stroke="#FF0101" fill="#FF0101" fillOpacity={0.7} />
-                  <Tooltip
-                  wrapperStyle={{outline:'none', border:'none'}}
-                  itemStyle={{color:'#000', fontSize:'10px'}}
-                  labelStyle={{color:'#000', display:'none'}}
-                  cursor={false}
-                  />
-              </RadarChart>
-          </ResponsiveContainer>
-      /*</article>*/
+    <ResponsiveContainer ref={containerRef} width="99%" height={263} className="polarchart-container">
+        <RadarChart cx="50%" cy="50%" innerRadius="0%" outerRadius="80%" data={perfDatas}>
+        <PolarGrid
+            radialLines={false} 
+            />
+            <PolarRadiusAxis
+            tickCount={6}
+            tick={false}
+            axisLine={false} 
+            tickLine={false}
+            />
+            <PolarAngleAxis 
+            dataKey="kind" 
+            axisLine={false} 
+            tickLine={false}
+            tick={<CustomPositionTicks containerWidth={containerWidth}/>}
+            /* tickFormatter={customEnduranceTick} */
+            /*tick={{ fontSize: '10px', fill:'#FFFFFF' }}*/
+            allowDuplicatedCategory={false}/>
+            <Radar dataKey="value" stroke="#FF0101" fill="#FF0101" fillOpacity={0.7} />
+            <Tooltip
+            wrapperStyle={{outline:'none', border:'none'}}
+            itemStyle={{color:'#000', fontSize:'10px'}}
+            labelStyle={{color:'#000', display:'none'}}
+            cursor={false}
+            />
+        </RadarChart>
+    </ResponsiveContainer>
   )
 }
 
--- src/components/ScoreChart.tsx
+++ src/components/ScoreChart.tsx
@@ -46,28 +46,26 @@ const ScoreChart = ({score} : props) => {
     // here, there is only one value so 0.3 is considered to represent a full circle
     // or we want 0.3 to be 360 / 0.3 deg so we use a polarangleaxis to scale it, setting the max to 1
     return(
-        /*<article>*/
-            <ResponsiveContainer ref={containerRef} width="99%" height={263} className="scorechart-container"> 
-                <RadialBarChart cx="50%" cy="50%" innerRadius="70%" outerRadius="80%" barSize={10} data={datas} startAngle={90} /* start at the top */ endAngle={450} /* anticlockwise since the attribute is deprecated */ > 
-                    <circle id="circleBG" cx="50%" cy="50%" r={containerWidth ? containerWidth/2*0.7 : 0} opacity="1" fill="white"/>
-                    <text x={12} y={14} fill="black" textAnchor="end" dominantBaseline="end">
-                        <tspan x="58" y="34" fontWeight={500} fontSize="15">Score</tspan>
-                    </text>
-                    <PolarAngleAxis type="number" domain={[0, 1]} angleAxisId={0} tick={false} />
-                    <RadialBar
-                    angleAxisId={0} 
-                    background
-                    dataKey="score"
-                    cornerRadius={10}
-                    />
-                    <text x={0} y={0} fill="black" textAnchor="middle" dominantBaseline="central">
-                        <tspan x="50%" y="42%" fontSize="26" fontWeight={600}>{datas[0].score ? datas[0].score*100 : 0}%</tspan>
-                        <tspan x="50%" dy="1.54rem" fontSize="16" fill="#74798C">de votre</tspan>
-                        <tspan x="50%" dy="1.4rem" fontSize="16" fill="#74798C">objectif</tspan>
-                    </text>
-                </RadialBarChart>
-            </ResponsiveContainer>
-        /*</article>*/
+        <ResponsiveContainer ref={containerRef} width="99%" height={263} className="scorechart-container"> 
+            <RadialBarChart cx="50%" cy="50%" innerRadius="70%" outerRadius="80%" barSize={10} data={datas} startAngle={90} /* start at the top */ endAngle={450} /* anticlockwise since the attribute is deprecated */ > 
+                <circle id="circleBG" cx="50%" cy="50%" r={containerWidth ? containerWidth/2*0.7 : 0} opacity="1" fill="white"/>
+                <text x={12} y={14} fill="black" textAnchor="end" dominantBaseline="end">
+                    <tspan x="58" y="34" fontWeight={500} fontSize="15">Score</tspan>
+                </text>
+                <PolarAngleAxis type="number" domain={[0, 1]} angleAxisId={0} tick={false} />
+                <RadialBar
+                angleAxisId={0} 
+                background
+                dataKey="score"
+                cornerRadius={10}
+                />
+                <text x={0} y={0} fill="black" textAnchor="middle" dominantBaseline="central">
+                    <tspan x="50%" y="42%" fontSize="26" fontWeight={600}>{datas[0].score ? datas[0].score*100 : 0}%</tspan>
+                    <tspan x="50%" dy="1.54rem" fontSize="16" fill="#74798C">de votre</tspan>
+                    <tspan x="50%" dy="1.4rem" fontSize="16" fill="#74798C">objectif</tspan>
+                </text>
+            </RadialBarChart>
+        </ResponsiveContainer>
         )
 }
 
