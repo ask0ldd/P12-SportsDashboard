e0f72b54acfdb18fbd6364dcd8f3243d1a60d2cb
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-23 15:39:37 +0200

    xaxis daily improved

--- src/components/DailyActivityChart.tsx
+++ src/components/DailyActivityChart.tsx
@@ -16,7 +16,7 @@ interface props {
  * @return ( <CustomXAxisTick/> )
  */
 const CustomXAxisTick = (value : number, index:number) : string => {
-    return (value+1).toString()
+    return (index+1).toString()
 }
 
 /**
@@ -75,65 +75,35 @@ const DailyActivityChart = ({dailyDatas} : props) => {
                     bottom: 48,
                 }}>
                     <Tooltip
-                    viewBox={{ x: 0, y: 0, width: 40, height: 65 }}
-                    wrapperStyle={{backgroundColor:"#E60000", outline:"none", border:"none"}}
-                    content={CustomTooltip}
-                    /* grey active bar hover */
-                    cursor={{opacity:0.5}}
-                    />
-                    <text x={12} y={30} fill="#20253A" textAnchor="middle" dominantBaseline="central"> {/* titre graph */}
+                    viewBox={{ x: 0, y: 0, width: 40, height: 65 }} wrapperStyle={{backgroundColor:"#E60000", outline:"none", border:"none"}}
+                    content={CustomTooltip} /* grey active bar hover */ cursor={{opacity:0.5}} />
+                    <text /* titre graph */ x={12} y={30} fill="#20253A" textAnchor="middle" dominantBaseline="central">
                         <tspan x="62" dy="0" fontSize="14" fontWeight={600}>Activité quotidienne</tspan>
                     </text>
-                    <CartesianGrid 
-                    strokeDasharray="2 2"
+
+                    <CartesianGrid  strokeDasharray="2 2"
                     /* only the horizontal lines of the grids are displayed */
-                    vertical={false} 
-                    stroke="#DEDEDE"
-                    />
-                    <XAxis 
-                    tickLine={false}
-                    width={1}
-                    domain={['dataMin', 'dataMax']}
-                    tickFormatter={CustomXAxisTick}
-                    stroke="#DEDEDE"
-                    tick={{ fill: '#9B9EAC' }}
-                    tickMargin={16}
-                    />
+                    vertical={false} stroke="#DEDEDE" />
+
+                    <XAxis tickLine={false} width={1} domain={['dataMin', 'dataMax']} dataKey="day"
+                    tickFormatter={CustomXAxisTick} stroke="#DEDEDE" tick={{ fill: '#9B9EAC' }} tickMargin={16} />
+
                     <YAxis 
-                    yAxisId={0} /* id kg Axis */
-                    dataKey="kilogram"
-                    type="number"
-                    domain={['dataMin-1', 'dataMax+2']} /* Y axis base value / max value */
-                    tickCount={3} /* 3 values on Y weight axis */
-                    tickLine={false} /* only numbers */
-                    axisLine={false}
-                    allowDecimals={false}
-                    orientation="right"
-                    tick={{ fill: '#9B9EAC' }}
-                    />
+                    yAxisId={0} /* id kg Axis */ dataKey="kilogram" type="number" domain={['dataMin-1', 'dataMax+2']} /* Y axis base value / max value */
+                    tickCount={3} /* 3 values on Y weight axis */ tickLine={false} /* only numbers */ axisLine={false}
+                    allowDecimals={false} orientation="right" tick={{ fill: '#9B9EAC' }} />
+
                     <YAxis 
-                    hide={true} /* don't need to be shown, only here to scale bars */
-                    yAxisId={1} /* id calories Axis */
-                    dataKey="calories"
-                    type="number"
-                    domain={['dataMin-100', 'dataMax+100']}
-                    allowDecimals={false}
-                    />
-                    <Legend 
-                    align="right"
-                    verticalAlign='top'
-                    width={290}
-                    iconSize={8}
-                    wrapperStyle={{top:8, right:24}}
+                    hide={true} /* don't need to be shown, only here to scale bars */ yAxisId={1} /* id calories Axis */
+                    dataKey="calories" type="number" domain={['dataMin-100', 'dataMax+100']} allowDecimals={false} />
+
+                    <Legend align="right" verticalAlign='top' width={290} iconSize={8} wrapperStyle={{top:8, right:24}}
                     payload={[{ value: 'Poids (kg)', type: 'circle', id: 'ID01', color: '#282D30'}, { value: 'Calories brûlées (kCal)', type: 'circle', id: 'ID02', color: '#E60000' }]}
-                    formatter={resizedLegendValue}
-                    />
-                    <Bar dataKey="kilogram" fill="#282D30" barSize={7} radius={[25, 25, 0, 0]}
-                    yAxisId={0} /* linked to this specific Y axis, determines how the bar is scaled */
-                    /> {/* barSize = bar width */}
-                    <Bar dataKey="calories" fill="#E60000" barSize={7} radius={[25, 25, 0, 0]}
-                    yAxisId={1}
-                    />
+                    formatter={resizedLegendValue} />
+
+                    <Bar dataKey="kilogram" fill="#282D30" barSize={7} /* barSize = bar width */ radius={[25, 25, 0, 0]}
+                    yAxisId={0} /* linked to this specific Y axis, determines how the bar is scaled */ />
+                    <Bar dataKey="calories" fill="#E60000" barSize={7} radius={[25, 25, 0, 0]} yAxisId={1}/>
                 </BarChart>
             </ResponsiveContainer>
         </article>
